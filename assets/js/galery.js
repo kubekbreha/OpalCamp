a8a24eb0edee937356f657fa5bd232bf36fbeaff
Jakub Brehuv <kubek.breha@gmail.com>
2018-06-01 08:44:38 +0200

    firebase basic functions

--- assets/js/galery.js
+++ assets/js/galery.js
@@ -1,6 +1,31 @@
-var MaterialPhotoGallery = require('material-photo-gallery');
-
-var elem = document.querySelector('.m-p-g');
-var gallery = new MaterialPhotoGallery(elem);
 
 
+// Get a reference to the database service
+var database = firebase.database();
+
+uploadNewPhoto("https://firebasestorage.googleapis.com/v0/b/opal-1526306570324.appspot.com/o/Start_2017%2FIMG_5261.jpg?alt=media&token=e745cd48-5c5b-486e-acf8-a47c0b342bff",'Start2017');
+
+
+
+
+function uploadNewPhoto(imageUrl, album) {
+    var refAlbum = database.ref(album);
+    refAlbum.push(imageUrl).then(function onSuccess(res) {
+        console.log(res)
+    }).catch(function onError(err) {
+        console.log(err)
+    });
+}
+
+
+loadPhotos('Start2017');
+
+function loadPhotos(album) {
+    var leadsRef = database.ref(album);
+    leadsRef.on("value", function (snapshot) {
+        snapshot.forEach(function (childSnapshot) {
+            var childData = childSnapshot.val();
+            console.log(childData);
+        });
+    });
+}
