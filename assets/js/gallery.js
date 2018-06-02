ab7967c01f909a3b9fe28eed76b7c3a6aa51e3af
Jakub Brehuv <kubek.breha@gmail.com>
2018-06-02 08:15:36 +0200

    correct image in index

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,36 +1,35 @@
 // Get a reference to the database service
-// var database = firebase.database();
-
-var database = firebase.database();
-
-function uploadNewPhoto(imageUrl, album) {
-    var refAlbum = database.ref(album);
-    refAlbum.push(imageUrl).then(function onSuccess(res) {
-        console.log(res)
-    }).catch(function onError(err) {
-        console.log(err)
-    });
-}
-
-function loadPhotos(album) {
-
-    var target = document.getElementById('test');
-    var leadsRef = database.ref('gallery/' + album);
-    leadsRef.on("value", function (snapshot) {
-        snapshot.forEach(function (childSnapshot) {
-            var childData = childSnapshot.val();
-            var  str = '<img style="height: 50%; width: 50%;" src="' + childData + '" data-full="' + childData + '" class="m-p-g__thumbs-img" />';
-            var temp = document.createElement('div');
-            temp.innerHTML = str;
-            while (temp.firstChild) {
-                target.appendChild(temp.firstChild);
-
-            }
-        });
-    });
-}
-
 $(document).ready(function () {
+
+    var database = firebase.database();
+
+    function uploadNewPhoto(imageUrl, album) {
+        var refAlbum = database.ref(album);
+        refAlbum.push(imageUrl).then(function onSuccess(res) {
+            console.log(res)
+        }).catch(function onError(err) {
+            console.log(err)
+        });
+    }
+
+    function loadPhotos(album) {
+
+        var target = document.getElementById('test');
+        var leadsRef = database.ref('gallery/' + album);
+        leadsRef.on("value", function (snapshot) {
+            snapshot.forEach(function (childSnapshot) {
+                var childData = childSnapshot.val();
+                var str = '<img  src="' + childData + '" data-full="' + childData + '" class="m-p-g__thumbs-img" />';
+                var temp = document.createElement('div');
+                temp.innerHTML = str;
+                while (temp.firstChild) {
+                    target.appendChild(temp.firstChild);
+
+                }
+            });
+        });
+    }
+
 
     loadPhotos('Start_2017');
 });
