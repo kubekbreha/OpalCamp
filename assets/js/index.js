ff56ab7136e2c4eab8a98b99cef39ff7fb5cabda
Jakub Brehuv <kubek.breha@gmail.com>
2018-06-04 15:03:12 +0200

    navbar for phone fixed

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,3 +2,8 @@ function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+
+function colapseSideNav() {
+    $("#navbar-toggler").click();
+}
